466999c616c890bf6b75bca082304a49fa596cff
Rafael Teixeira <monoman@gmail.com>
2014-11-26 11:52:41 -0200

    fix script

--- pullconflicts.cmd
+++ pullconflicts.cmd
@@ -3,6 +3,6 @@ git branch -d mono-gh-pages
 git pull
 git checkout -b mono-gh-pages gh-pages
 git pull https://github.com/mono/website.git gh-pages
-git status | grep "both modified" | cut -c19- | sed -e 's/\//\\/g' > .conflicts
+git status | grep "both modified" | cut -c19- > .conflicts
 cat .conflicts
 FOR /F %%p IN ('cat .conflicts') DO "C:\Program Files (x86)\Notepad++\notepad++.exe" %%p
