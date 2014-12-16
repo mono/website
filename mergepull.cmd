82a5aa3ed570969390f6484dc3f49e009ea23c46
Rafael Teixeira <monoman@gmail.com>
2014-12-16 14:38:53 -0200

    cleaner output

--- mergepull.cmd
+++ mergepull.cmd
@@ -1,15 +1,15 @@
 git fetch origin gh-pages
-if errorlevel 1 goto :END
+@if errorlevel 1 goto :END
 git rebase origin/gh-pages
-if errorlevel 1 goto :END
+@if errorlevel 1 goto :END
 git checkout gh-pages
-if errorlevel 1 goto :END
+@if errorlevel 1 goto :END
 git pull
-if errorlevel 1 goto :END
+@if errorlevel 1 goto :END
 git rebase pullrequest
-if errorlevel 1 goto :END
+@if errorlevel 1 goto :END
 git push origin gh-pages
-if errorlevel 1 goto :END
+@if errorlevel 1 goto :END
 @goto :ENDEND
 :END
 @echo Failed!!!
