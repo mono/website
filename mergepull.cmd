680f28f158f31d042bc9fd3b74fd4c72967d3c97
Rafael Teixeira <monoman@gmail.com>
2014-12-16 14:35:17 -0200

    Better error handling

--- mergepull.cmd
+++ mergepull.cmd
@@ -1,7 +1,16 @@
 git fetch origin gh-pages
+if errorlevel 1 goto :END
 git rebase origin/gh-pages
+if errorlevel 1 goto :END
 git checkout gh-pages
+if errorlevel 1 goto :END
 git pull
+if errorlevel 1 goto :END
 git rebase pullrequest
-rem git merge --no-ff mono-gh-pages
+if errorlevel 1 goto :END
 git push origin gh-pages
+if errorlevel 1 goto :END
+@goto :ENDEND
+:END
+@echo Failed!!!
+:ENDEND
--- pullconflicts.cmd
+++ pullconflicts.cmd
@@ -1,15 +1,25 @@
-@if [%1]==[] goto :END
+@if [%1]==[] goto :FAIL
 git checkout gh-pages
+if errorlevel 1 goto :END
 git branch -D pullrequest
 git pull
+if errorlevel 1 goto :END
 git checkout -b pullrequest gh-pages
+if errorlevel 1 goto :END
 git pull https://github.com/%1/website.git gh-pages
+if errorlevel 1 goto :END
 git status | grep "both modified" | cut -c19- > .conflicts
+if errorlevel 1 goto :END
 cat .conflicts
 FOR /F %%p IN ('cat .conflicts') DO "C:\Program Files (x86)\Notepad++\notepad++.exe" %%p
+if errorlevel 1 goto :END
 git add .
+if errorlevel 1 goto :END
 git commit -m "Fix conflicts"
+if errorlevel 1 goto :END
 @goto :ENDEND
-:END
+:FAIL
 @echo You need to pass the PR github account to make the pull
+:END
+@echo Failed!!!
 :ENDEND
