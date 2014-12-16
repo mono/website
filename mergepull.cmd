fcfbe1056cd2c0536afffafafb8d0c9953e36728
Rafael Teixeira <monoman@gmail.com>
2014-12-16 13:25:48 -0200

    fixed pullrequest branch name

--- mergepull.cmd
+++ mergepull.cmd
@@ -2,6 +2,6 @@ git fetch origin gh-pages
 git rebase origin/gh-pages
 git checkout gh-pages
 git pull
-git rebase mono-gh-pages
+git rebase pullrequest
 rem git merge --no-ff mono-gh-pages
 git push origin gh-pages
--- pullconflicts.cmd
+++ pullconflicts.cmd
@@ -1,9 +1,8 @@
-set WHERE=%1 
 git checkout gh-pages
-git branch -D %WHERE%-gh-pages
+git branch -D pullrequest
 git pull
-git checkout -b %WHERE%-gh-pages gh-pages
-git pull https://github.com/%WHERE%/website.git gh-pages
+git checkout -b pullrequest gh-pages
+git pull https://github.com/%1/website.git gh-pages
 git status | grep "both modified" | cut -c19- > .conflicts
 cat .conflicts
 FOR /F %%p IN ('cat .conflicts') DO "C:\Program Files (x86)\Notepad++\notepad++.exe" %%p
